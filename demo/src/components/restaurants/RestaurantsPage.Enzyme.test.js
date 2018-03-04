e3b9fd1ee3799649ae4630f316cdd1e53f3ada8d
Christine Dai <christinedai@Christines-MacBook-Air.local>
2018-03-03 20:29:27 -0800

    added tests for restaurants

---
+++ demo/src/components/restaurants/RestaurantsPage.Enzyme.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import expect from 'expect';
+import {mount, shallow, render} from 'enzyme';
+import TestUtils from 'react-addons-test-utils';
+
+import RestaurantsPage from "./RestaurantsPage";
+import Wrapper from "./Wrapper";
+import Restaurant from "./Restaurant";
+
+function setup(saving) {
+    const props = {
+
+    };
+}
+
+// shallow: only goes 1 level deep (no restaurants)
+// mount: renders everything
+describe("component RestaurantsPage", () => {
+    it("should have 3 restaurants at the beginning", () => {
+        const wrapper = mount(<RestaurantsPage />);
+        expect(wrapper.find(Wrapper).find(Restaurant).length).toBe(3);
+    });
+
+    it("should, after clicking the button, 3 more restaurants should appear", () => {
+        const wrapper = mount(<RestaurantsPage/>);
+        wrapper.find(".nextbutton").simulate("click");
+        expect(wrapper.find(Wrapper).find(Restaurant).length).toBe(6);
+    }); 
+});
