810ad9b6a6e25e966b3140fe9afffa18a15a181f
Christine Dai <christinedai@christines-air.dynamic.ucsd.edu>
2018-03-04 02:57:53 -0800

    Made components for restaurantDetail and wrote some tests for Cart; cant tell if tests work or not since Im getting a weird error on my side so I commented the tests out for now

---
+++ demo/src/components/cart/Cart.Enzyme.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import expect from 'expect';
+import {mount, shallow, render} from 'enzyme';
+import TestUtils from 'react-addons-test-utils';
+
+import Cart from "./Cart";
+import CartList from "./CartList";
+import Dish from "./Dish";
+import Total from "./Total";
+
+describe("component Cart", () => {
+    /*
+    it("should start with 3 dishes", () => {
+        const wrapper = mount(<Cart/>);
+        expect(wrapper.find(Dish).length).toBe(3);
+    });
+
+    it("after removing once, should have 2 dishes", () => {
+        const wrapper = mount(<Cart/>);
+        wrapper.find(".removeItem").find("button").simulate("click");
+        expect(wrapper.find(Dish).length).toBe(2);
+    });
+    */
+});
